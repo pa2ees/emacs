a9ee20ff522de23c721b0d1ab58a78063a621fec
Erik van Zwol <pa2ees42@gmail.com>
2020-09-15 23:08:39 -0600

    turned off pdf stuff, pushed dired stuff to its own file and updated instructions

---
+++ init_dired.el
@@ -0,0 +1,197 @@
+;; This advises functions to be aware of subdirs in dired mode
+(defun dired-subdir-aware (orig-fun &rest args)
+  (if (eq major-mode 'dired-mode)
+      (let ((default-directory (dired-current-directory)))
+        (apply orig-fun args))
+    (apply orig-fun args)))
+
+(advice-add 'read-file-name :around 'dired-subdir-aware)
+(advice-add 'find-file-read-args :around 'dired-subdir-aware)
+(advice-add 'dired-do-compress-to :around 'dired-subdir-aware)
+
+(defun dired-sort-set-mode-line-extended ()
+  (when (eq major-mode 'dired-mode)
+    (setq mode-name
+          (let ((sorting-reversed (string-match-p (dired-get-match-string-for-switch "r") dired-actual-switches))
+                case-fold-search)
+            (let ((rev-str (if sorting-reversed
+                               " Rev "
+                             " ")))
+
+              (cond ((string-match-p
+                      (dired-get-match-string-for-switch "X") dired-actual-switches)
+                     (concat "Dired by Ext" rev-str))
+                    ((string-match-p
+                      (dired-get-match-string-for-switch "U") dired-actual-switches)
+                     (concat "Dired by dir order" rev-str))
+                    ((string-match-p
+                      (dired-get-match-string-for-switch "S") dired-actual-switches)
+                     (concat "Dired by Size" rev-str))
+                    ((string-match-p
+                      (dired-get-match-string-for-switch "t") dired-actual-switches)
+                     (concat "Dired by Date" rev-str))
+                    (t
+                     (concat "Dired by Name" rev-str))))))
+    (force-mode-line-update)))
+
+(advice-add 'dired-sort-set-mode-line :override 'dired-sort-set-mode-line-extended)
+
+
+(defun dired-remove-sort-all ()
+  (progn
+    (dired-remove-sort "t")
+    (dired-remove-sort "S")
+    (dired-remove-sort "X")
+    (dired-remove-sort "U")))
+
+(defun dired-get-match-string-for-switch (arg)
+  (if (> (string-to-char arg) (string-to-char "Z"))
+      (concat "\\(\\`\\| \\)-\\([a-"
+              (char-to-string (- (string-to-char arg) 1))
+              (char-to-string (+ (string-to-char arg) 1))
+              "-zA-Z]*\\)\\("
+              arg
+              "\\)\\([^ ]*\\)")
+    (concat "\\(\\`\\| \\)-\\([a-"
+            (char-to-string (- (string-to-char arg) 1))
+            (char-to-string (+ (string-to-char arg) 1))
+            "-zA-Z]*\\)\\("
+            arg
+            "\\)\\([^ ]*\\)")))
+
+(defun dired-remove-switch (arg)
+  (let ((switch-regexp (dired-get-match-string-for-switch arg))
+                       case-fold-search)
+    ;; Remove the switch.
+    (while (string-match switch-regexp dired-actual-switches)
+      (if (and (equal (match-string 2 dired-actual-switches) "")
+               (equal (match-string 4 dired-actual-switches) ""))
+          ;; Remove a stand-alone switch.
+          (progn
+            (setq dired-actual-switches
+                  (replace-match "" t t dired-actual-switches)))
+        ;; Remove a switch of the form -XaY for some X and Y where a is the switch to be removed.
+        (setq dired-actual-switches
+              (replace-match "" t t dired-actual-switches 3))))))
+
+(defun dired-add-switch (arg)
+  (let ((switch-regexp "\\(.*?\\)\\(\\`\\| +\\)\\(-[[:alnum:]]+\\)\\(.*\\)"))
+    (setq dired-actual-switches
+          (if (string-match switch-regexp dired-actual-switches)
+              (concat (match-string 1 dired-actual-switches)
+                      (match-string 2 dired-actual-switches)
+                      (match-string 3 dired-actual-switches)
+                      arg
+                      (match-string 4 dired-actual-switches))
+            (concat dired-actual-switches
+                    " -"
+                    arg))))
+  (dired-sort-set-mode-line))
+
+(defun dired-switch-toggle (arg)
+  (let ((switch-exists (string-match-p (dired-get-match-string-for-switch arg) dired-actual-switches))
+        case-fold-search)
+    (if switch-exists
+        (progn
+          (dired-remove-switch arg))
+      (dired-add-switch arg))))
+
+
+(defun dired-do-sort (arg)
+  (dired-add-switch arg))
+  
+(defun dired-remove-sort (arg)
+  (dired-remove-switch arg))
+
+(defun dired-sort-file-extension ()
+  (interactive)
+  (dired-remove-sort-all)
+  (dired-do-sort "X")
+  (revert-buffer)
+  (message "Sorting by File Extension"))
+
+(defun dired-sort-filename ()
+  (interactive)
+  (dired-remove-sort-all)
+  (dired-sort-set-mode-line)
+  (revert-buffer)
+  (message "Sorting by Filename"))
+
+(defun dired-sort-directory-order ()
+  (interactive)
+  (dired-remove-sort-all)
+  (dired-do-sort "U")
+  (revert-buffer)
+  (message "Sorting by Directory Order"))
+
+(defun dired-sort-size ()
+  (interactive)
+  (dired-remove-sort-all)
+  (dired-do-sort "S")
+  (revert-buffer)
+  (message "Sorting by File Size"))
+
+(defun dired-sort-time ()
+  (interactive)
+  (dired-remove-sort-all)
+  (dired-do-sort "t")
+  (revert-buffer)
+  (message "Sorting by File Modification Date/Time"))
+
+(defun dired-sort-reversed-toggle ()
+  (dired-switch-toggle "r")
+  (dired-sort-set-mode-line)
+  (revert-buffer)
+  (message "Toggled Reverse Sorting"))
+
+(defun dired-reset-switches ()
+  (setq dired-actual-switches "-Blh --group-directories-first")
+  (dired-sort-set-mode-line)
+  (revert-buffer)
+  (message "Reset dired switches"))
+
+(defun dired-show-all-toggle ()
+  (dired-remove-switch "B")
+  (dired-remove-switch "A")
+  (dired-switch-toggle "a")
+  (revert-buffer))
+
+(defun dired-human-readable-toggle ()
+  (dired-switch-toggle "h")
+  (revert-buffer))
+
+(defun dired-owner-toggle ()
+  (dired-switch-toggle "g")
+  (revert-buffer))
+
+(defun dired-group-toggle ()
+  (dired-switch-toggle "G")
+  (revert-buffer))
+
+(defun dired-backup-toggle ()
+  (dired-switch-toggle "B")
+  (revert-buffer))
+
+  
+(add-hook 'dired-mode-hook (lambda ()
+                             (dired-reset-switches)
+                             (local-set-key (kbd "M-s s x") (lambda() (interactive) (dired-sort-file-extension)))
+                             (local-set-key (kbd "M-s s D") (lambda() (interactive) (dired-sort-directory-order)))
+                             (local-set-key (kbd "M-s s f") (lambda() (interactive) (dired-sort-filename)))
+                             (local-set-key (kbd "M-s s n") (lambda() (interactive) (dired-sort-filename)))
+                             (local-set-key (kbd "M-s s s") (lambda() (interactive) (dired-sort-size)))
+                             (local-set-key (kbd "M-s s t") (lambda() (interactive) (dired-sort-time)))
+                             (local-set-key (kbd "M-s s d") (lambda() (interactive) (dired-sort-time)))
+                             (local-set-key (kbd "M-s s r") (lambda() (interactive) (dired-sort-reversed-toggle)))
+                             (local-set-key (kbd "M-s r") (lambda() (interactive) (dired-reset-switches)))
+                             (local-set-key (kbd "M-s a") (lambda() (interactive) (dired-show-all-toggle)))
+                             (local-set-key (kbd "M-s H") (lambda() (interactive) (dired-human-readable-toggle)))
+                             (local-set-key (kbd "M-s g") (lambda() (interactive) (dired-owner-toggle)))
+                             (local-set-key (kbd "M-s G") (lambda() (interactive) (dired-group-toggle)))
+                             (local-set-key (kbd "M-s b") (lambda() (interactive) (dired-backup-toggle)))
+                             
+                             (linum-mode -1)))
+
+
+;; (add-hook 'diredfl-mode-hook (lambda ()
+;;                                (
