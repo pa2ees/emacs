3beeac330e88b95511716c5304f6d92985b1cafa
Erik van Zwol <erik.vanzwol@imsar.com>
2022-07-06 21:49:08 -0600

    Add ediff copy both to c function

--- init_magit.el
+++ init_magit.el
@@ -16,3 +16,12 @@
           (insert (match-string 1 branch-name))))))
       
 (add-hook 'git-commit-setup-hook #'evz/git-message-add-jira-ticket)
+
+(defun evz/ediff-copy-both-to-C ()
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+                   (concat
+                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+(defun evz/add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'evz/ediff-copy-both-to-C))
+(add-hook 'ediff-keymap-setup-hook #'evz/add-d-to-ediff-mode-map)
